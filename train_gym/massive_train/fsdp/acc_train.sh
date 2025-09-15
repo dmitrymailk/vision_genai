1d26be7a295a912bb930f18c591d2d65c18eb761
_dimweb_ <dimweb.tech@mail.ru>
2025-09-15 02:13:50 +0000

    add flow

--- train_gym/massive_train/fsdp/acc_train.sh
+++ train_gym/massive_train/fsdp/acc_train.sh
@@ -1,3 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0,1
+# export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0
 config_path=/code/train_gym/massive_train/fsdp/fsdp2_default_config.yaml
 accelerate launch --config_file $config_path accelerate_nlp_example.py
